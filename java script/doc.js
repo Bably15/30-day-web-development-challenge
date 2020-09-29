3c18714b2c6e6f0e883711541bb7fa1159d54631
Bably15 <bablyd15@gmail.com>
2020-09-29 23:23:47 +0530

    java script

---
+++ java script/doc.js
@@ -0,0 +1,19 @@
+alert('hello world');
+let myVariable1;
+myVariable1 = 20;
+myVariable1 + 5;
+
+console.log(myVariable1);
+
+let youLikeMeat = true;
+
+if (youLikeMeat) {
+	document.write('you like meat');
+} else {
+	document.write("you don't like meat");
+}
+let num = 1;
+
+if (num > 2) {
+	document.write(' 5 is greater than 2');
+}
